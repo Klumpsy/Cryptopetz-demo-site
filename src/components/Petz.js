c62ace924ac243cc381eff1b0b4638be86867c0f
bart <bart_klumperman@Live.nl>
2021-09-30 22:37:20 +0200

    implemented API call for 10000 cryptopetz

--- src/components/Petz.js
+++ src/components/Petz.js
@@ -1,12 +1,53 @@
 import allpetz from "../images/allpetz.jpg";
+import React, {useState, useEffect} from 'react'; 
+import axios from 'axios';
 
 const Petz = () => { 
+    const [petz, setPetz] = useState([]); 
+
+    useEffect(()=> {
+        try{
+            async function fetchData() { 
+                const request = await axios.get("/all");
+                setPetz(request.data)
+                return request;
+            }
+    
+            fetchData();
+        }
+        catch(err) {
+            return err => console.log(err)
+        }
+     
+    }, []);
+
+   const petzArray = [];
+
+    for (let[key, value] of Object.entries(petz)) { 
+            petzArray.push(value);
+        }
+
     return (
-        <div>
-            <div className ="petz-wrapper">
-                 <img className = "petz-wrapper-banner" src= {allpetz}/>
+        
+            <div className ="petz-container">  
+                <div className="petz-search-input">
+                    <input placeholder="Search pet by Type, Rarity or ID..."/>
+                </div>
+                <div className ="petz-wrapper" >
+                {petzArray.map(pet => ( 
+                    <div className= {`pet-card ${pet.data.types[0]}`} pet-card key={pet.name}>
+                        <h4>{pet.name}</h4>
+                        <img src= {`https://cryptopetz.info${pet.image}`}/>
+                        <div>
+                            <p>Gen: {pet.data.generation}</p>
+                            <p>Type: {pet.data.types}</p>
+                            <p>Rarity: {pet.data.rarity}</p>
+                        </div>
+                    </div>
+                ))}
+                </div>
             </div>
-        </div>
+               
     )
 }
 
