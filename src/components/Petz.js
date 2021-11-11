82e34717fff1120f25a8b083d6f4164c99621aa0
bart <bart_klumperman@Live.nl>
2021-11-11 22:51:50 +0100

    added cards to petz collection legendary only and basic css setup

--- src/components/Petz.js
+++ src/components/Petz.js
@@ -1,10 +1,33 @@
-
+import { EpochOneLegendary } from "./PetzData/epochOnePetz";
 
 const Petz = () => { 
     return (
         <div className ="all-petz-container">
+            <h1>Epoch 1</h1>
+            <h2 className = "legendary">Legendary</h2>
             <div className ="legendary-petz">
-
+                { 
+                    EpochOneLegendary.map(pet => (
+                        <div className =  " epoch-one-pet-card">
+                            <img src={pet.image}></img>
+                            <div>
+                                <span style ={{color: "white"}}>Rarity: </span><span className={pet.rarity}>{pet.rarity}</span>
+                            </div>
+                            <div style ={{flexDirection: "column"}}>
+                                <span style ={{color: "white"}}>Type: </span>
+                                {pet.type.length > 1 ?
+                                <div className = "pet-card-type-container">
+                                    <span className ={pet.type[0]}>{pet.type[0]}</span>
+                                    <span className ={pet.type[1]}>{pet.type[1]}</span>
+                                </div> 
+                                :
+                                <div className = "pet-card-type-container">
+                                    <span className ={pet.type[0]}>{pet.type[0]}</span>
+                                </div>}
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
---
+++ src/components/PetzData/epochOnePetz.js
@@ -0,0 +1,50 @@
+//Legendary Petz
+import cryptoPet1 from "../../images/smallPngPetz/Pi7compressed01.png"
+import cryptoPet2 from "../../images/smallPngPetz/Pi7compressed02.png"
+import cryptoPet3 from "../../images/smallPngPetz/Pi7compressed03.png"
+import cryptoPet4 from "../../images/smallPngPetz/Pi7compressed04.png"
+import cryptoPet5 from "../../images/smallPngPetz/Pi7compressed05.png"
+
+
+//Legendary Object
+export const EpochOneLegendary =
+[
+    { 
+        image: cryptoPet1,
+        name: "CryptoPet1",
+        rarity: "legendary",
+        type: ["flying", "grass"]
+    },
+    { 
+        image: cryptoPet2,
+        name: "CryptoPet2",
+        rarity: "legendary",
+        type: ["combat"]
+    },
+    { 
+        image: cryptoPet3,
+        name: "CryptoPet3",
+        rarity: "legendary",
+        type: ["spirit", "ice"]
+    },
+    { 
+        image: cryptoPet4,
+        name: "CryptoPet4",
+        rarity: "legendary",
+        type: ["flying", "insect"]
+    },
+    { 
+        image: cryptoPet5,
+        name: "CryptoPet5",
+        rarity: "legendary",
+        type: ["earth"]
+    }
+]
+//Ultra rare Petz
+//Extremely rare Petz 
+//Epic Petz
+//Rare Petz
+//Scarce Petz
+//Uncommmon Petz
+//Common Petz
+//Very Common Petz
