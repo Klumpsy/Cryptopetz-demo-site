6ed1f6cf6de25c6aa514c8d6b604646637113709
bart <bart_klumperman@Live.nl>
2021-11-12 17:43:04 +0100

    added new petz object

--- src/components/Petz.js
+++ src/components/Petz.js
@@ -1,4 +1,4 @@
-import { EpochOneLegendary } from "./PetzData/epochOnePetz";
+import { EpochOneLegendary, EpochOneUltraRare } from "./PetzData/epochOnePetz";
 
 const Petz = () => { 
     return (
@@ -29,6 +29,38 @@ const Petz = () => {
                     ))
                 }
             </div>
+            <h2 className = "ultrarare">Ultra Rare</h2>
+            <div className ="legendary-petz">
+                { 
+                    EpochOneUltraRare.map(pet => (
+                        <div className =  " epoch-one-pet-card">
+                            <img src={pet.image}></img>
+                            <div>
+                                <span style ={{color: "white"}}>Rarity: </span><span className={pet.rarity}>{pet.rarity}</span>
+                            </div>
+                            <div style ={{flexDirection: "column"}}>
+                                <span style ={{color: "white"}}>Type: </span>
+                                {pet.type.length > 1 ?
+                                <div className = "pet-card-type-container">
+                                    <span className ={pet.type[0]}>{pet.type[0]}</span>
+                                    <span className ={pet.type[1]}>{pet.type[1]}</span>
+                                </div> 
+                                :
+                                <div className = "pet-card-type-container">
+                                    <span className ={pet.type[0]}>{pet.type[0]}</span>
+                                </div>}
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+            <h2 className = "extremelyrare">Extremely Rare</h2>
+            <h2 className = "epic">Epic</h2>
+            <h2 className = "rare">Rare</h2>
+            <h2 className = "scarce">Scarce</h2>
+            <h2 className = "uncommon">Uncommon</h2>
+            <h2 className = "common">Common</h2>
+            <h2 className = "verycommon">Very Common</h2>
         </div>
     )
 }
--- src/components/PetzData/epochOnePetz.js
+++ src/components/PetzData/epochOnePetz.js
@@ -4,6 +4,25 @@ import cryptoPet2 from "../../images/smallPngPetz/Pi7compressed02.png"
 import cryptoPet3 from "../../images/smallPngPetz/Pi7compressed03.png"
 import cryptoPet4 from "../../images/smallPngPetz/Pi7compressed04.png"
 import cryptoPet5 from "../../images/smallPngPetz/Pi7compressed05.png"
+//Ultra Rare Petz 
+import cryptoPet6 from "../../images/smallPngPetz/Pi7compressed021.png"
+import cryptoPet7 from "../../images/smallPngPetz/Pi7compressed024.png"
+import cryptoPet8 from "../../images/smallPngPetz/Pi7compressed028.png"
+import cryptoPet9 from "../../images/smallPngPetz/Pi7compressed09.png"
+import cryptoPet10 from "../../images/smallPngPetz/Pi7compressed040.png"
+import cryptoPet11 from "../../images/smallPngPetz/Pi7compressed044.png"
+import cryptoPet12 from "../../images/smallPngPetz/Pi7compressed046.png"
+import cryptoPet13 from "../../images/smallPngPetz/Pi7compressed052.png"
+import cryptoPet14 from "../../images/smallPngPetz/Pi7compressed059.png"
+import cryptoPet15 from "../../images/smallPngPetz/Pi7compressed075.png"
+import cryptoPet16 from "../../images/smallPngPetz/Pi7compressed089.png"
+import cryptoPet17 from "../../images/smallPngPetz/Pi7compressed093.png"
+import cryptoPet18 from "../../images/smallPngPetz/Pi7compressed095.png"
+import cryptoPet19 from "../../images/smallPngPetz/Pi7compressed0104.png"
+import cryptoPet20 from "../../images/smallPngPetz/Pi7compressed0109.png"
+import cryptoPet21 from "../../images/smallPngPetz/Pi7compressed0115.png"
+import cryptoPet22 from "../../images/smallPngPetz/Pi7compressed0122.png"
+//Extremely Rare Petz
 
 
 //Legendary Object
@@ -41,6 +60,111 @@ export const EpochOneLegendary =
     }
 ]
 //Ultra rare Petz
+export const EpochOneUltraRare =
+[
+    { 
+        image: cryptoPet6,
+        name: "CryptoPet6",
+        rarity: "ultrarare",
+        type: ["psychic", "drake"]
+    },
+    { 
+        image: cryptoPet7,
+        name: "CryptoPet7",
+        rarity: "ultrarare",
+        type: ["fire", "drake"]
+    },
+    { 
+        image: cryptoPet8,
+        name: "CryptoPet8",
+        rarity: "ultrarare",
+        type: ["electric", "drake"]
+    },
+    { 
+        image: cryptoPet9,
+        name: "CryptoPet9",
+        rarity: "ultrarare",
+        type: ["insect"]
+    },
+    { 
+        image: cryptoPet10,
+        name: "CryptoPet10",
+        rarity: "ultrarare",
+        type: ["fae", "water"]
+    },
+    { 
+        image: cryptoPet11,
+        name: "CryptoPet11",
+        rarity: "ultrarare",
+        type: ["combat", "earth"]
+    },
+    { 
+        image: cryptoPet12,
+        name: "CryptoPet12",
+        rarity: "ultrarare",
+        type: ["combat", "metal"]
+    },
+    { 
+        image: cryptoPet13,
+        name: "CryptoPet13",
+        rarity: "ultrarare",
+        type: ["fire"]
+    },
+    { 
+        image: cryptoPet14,
+        name: "CryptoPet14",
+        rarity: "ultrarare",
+        type: ["sound", "spirit"]
+    },
+    { 
+        image: cryptoPet15,
+        name: "CryptoPet15",
+        rarity: "ultrarare",
+        type: ["ice", "flying"]
+    },
+    { 
+        image: cryptoPet16,
+        name: "CryptoPet16",
+        rarity: "ultrarare",
+        type: ["shadow", "lava"]
+    },
+    { 
+        image: cryptoPet17,
+        name: "CryptoPet17",
+        rarity: "ultrarare",
+        type: ["lava", "poison"]
+    },
+    { 
+        image: cryptoPet18,
+        name: "CryptoPet18",
+        rarity: "ultrarare",
+        type: ["lava", "drake"]
+    },
+    { 
+        image: cryptoPet19,
+        name: "CryptoPet19",
+        rarity: "ultrarare",
+        type: ["psychic", "drake"]
+    },
+    { 
+        image: cryptoPet20,
+        name: "CryptoPet20",
+        rarity: "ultrarare",
+        type: ["psychic", "drake"]
+    },
+    { 
+        image: cryptoPet21,
+        name: "CryptoPet21",
+        rarity: "ultrarare",
+        type: ["earth", "spirit"]
+    },
+    { 
+        image: cryptoPet22,
+        name: "CryptoPet22",
+        rarity: "ultrarare",
+        type: ["sound"]
+    }
+]
 //Extremely rare Petz 
 //Epic Petz
 //Rare Petz
