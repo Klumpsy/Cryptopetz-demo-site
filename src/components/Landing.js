4eec28503e1f018ef784842a76b7959a58069ec0
bart <bart_klumperman@Live.nl>
2021-11-16 22:43:52 +0100

    seperated logo from background homepage with MQ

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+import logo from "../images/petz_logo.png"
 
 const Landing = () => { 
   
     return (
         <div className ="landing-container">
+            <img src ={logo}/>
         </div>
     );
 }
--- src/components/Updates.js
+++ src/components/Updates.js
@@ -6,20 +6,49 @@ import UpdateComponentThree from "./updates/UpdateThree";
 import UpdateComponentTwo from "./updates/UpdateTwo";
 import UpdateComponentOne from "./updates/UpdateOne";
 
+import { useState } from "react";
+
 const Updates = ({ allPetzArray }) => { 
 
+    const updates = 
+    [
+        <UpdateComponentSeven allPetzArray = {allPetzArray}/>,
+        <UpdateComponentSix allPetzArray = {allPetzArray}/>,
+        <UpdateComponentFive allPetzArray = {allPetzArray}/>,
+        <UpdateComponentFour allPetzArray = {allPetzArray}/>,
+        <UpdateComponentThree allPetzArray = {allPetzArray}/>,
+        <UpdateComponentTwo allPetzArray = {allPetzArray}/>,
+        <UpdateComponentOne allPetzArray = {allPetzArray}/>
+    ];
+
+    const [currentUpdate, setCurrentUpdate] = useState(0);
+    const length = updates.length
+
+    const nextUpdate = () => { 
+        setCurrentUpdate(currentUpdate === length -1 ? 0 : currentUpdate + 1)
+    }
+
+    const previousUpdate = () => { 
+        setCurrentUpdate(currentUpdate === 0 ? length -1 : currentUpdate - 1)
+    }
 
     return (
         <div className = "updates-background">
             <div className = "update-page-wrapper"> 
                 <h1 className="updates-title">Updates</h1>
-                <UpdateComponentSeven allPetzArray = {allPetzArray}/>
-                <UpdateComponentSix allPetzArray = {allPetzArray}/>
-                <UpdateComponentFive allPetzArray = {allPetzArray}/>
-                <UpdateComponentFour allPetzArray = {allPetzArray}/>
-                <UpdateComponentThree allPetzArray = {allPetzArray}/>
-                <UpdateComponentTwo allPetzArray = {allPetzArray}/>
-                <UpdateComponentOne allPetzArray = {allPetzArray}/>
+                <div className ="arrow-holder">
+                        <i className= {currentUpdate === 0 ? "" : "updates-arrow updates-arrow-left fa fas fa-arrow-circle-left fa-4x"} onClick ={previousUpdate}></i>
+                            {
+                                updates.map((update, index) => {
+                                return (
+                                    <div className ={index === currentUpdate ? 'update-active': 'update'} key={index}>
+                                        {index === currentUpdate ? update : ""}
+                                    </div>
+                                )
+                            })
+                        }
+                        <i className= {currentUpdate === (updates.length -1) ? "" : "updates-arrow updates-arrow-right fa fas fa-arrow-circle-right fa-4x"} onClick = {nextUpdate}></i>
+                </div>
             </div>
         </div>
     )
