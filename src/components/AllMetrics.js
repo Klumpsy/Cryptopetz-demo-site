c8acdad7a4f59e855f6dfc3e467aa914b9f84b73
bart <bart_klumperman@Live.nl>
2021-12-02 19:31:57 +0100

    added new gif

--- src/components/AllMetrics.js
+++ src/components/AllMetrics.js
@@ -20,9 +20,6 @@ const AllMetrics = ({mode, width}) => {
                             <div className="all-metrics-box-background">
                                 <div className="all-metrics-box metrics-box-rarity">
                                 <h1 className ="rarity-metrics-title">Rarity Metrics</h1>
-                                <p>Petz have 10 different raritys and way more extra rarity metrics!
-                                   The CryptoPetz collection has a total of 21 different types.
-                                   Some CryptoPetz may have up to 2 types each. Checkout all the different metrics here.</p>
                                 </div>
                             </div>
                         </Link>
@@ -32,11 +29,6 @@ const AllMetrics = ({mode, width}) => {
                             <div className="all-metrics-box-background">
                                 <div className="all-metrics-box metrics-box-petz">
                                     <h1 className ="petz-metrics-title">Petz Metrics</h1>
-                                    <p>The strengths and weaknesses are displayed as Radar charts.
-                                       The numbers present on the charts are multipliers.
-                                       <br/><br/>
-                                       For example, if the multiplier number is 2, the type receiving the damage will be
-                                       double what it would usually receive if it were type with a multiplier value of 1.</p>
                                 </div>
                             </div>
                         </Link>
@@ -44,13 +36,8 @@ const AllMetrics = ({mode, width}) => {
                     <div className= "metrics-box metrics-box-game-info">
                         <Link to ="/GameInfo" style={{ textDecoration: 'none' }}>
                             <div className="all-metrics-box-background">
-                                <div className="all-metrics-box">
+                                <div className="all-metrics-box metrics-box-game-info">
                                 <h1 className ="game-info-title">Game Metrics</h1>
-                                <p>CryptoPetz The Game - is a tournament-style online role-playing game (RPG)
-                                    where you can collect and enhance your own CryptoPet.
-                                    Form your own team and battle others. Every CryptoPetz has a number of attribute slots,
-                                    ability slots and special attack slots that can be used by their respective tokens purchased
-                                    through the store (coming soon).</p>
                                 </div>
                             </div>
                         </Link>
